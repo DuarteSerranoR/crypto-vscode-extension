e42f3e3236926d95501d4b41c7c363b010284d87
Duarte R <raposoduca@gmail.com>
2022-08-12 01:46:07 +0100

    imported wasm into extension, not working yet

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,3 +1,19 @@
-//mod crypto;
+
+
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 pub mod crypto_static;
 pub use crypto_static::encrypt;
+
+#[wasm_bindgen]
+pub fn rust_encrypt(message: String) -> String {
+    return encrypt(message);
+}
